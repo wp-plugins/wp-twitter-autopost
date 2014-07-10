876780e699d39715fa44238131ac5aa6aef17459
vivacityinfotech.jaipur <vivacityinfotech.jaipur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-10 19:11:48 +0000

    Donation link updated
    git-svn-id: https://plugins.svn.wordpress.org/wp-twitter-autopost/trunk@946447 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/wptp-twitter_config.php
+++ inc/wptp-twitter_config.php
@@ -265,14 +265,17 @@ function wptp_get_sidebar() {
 			
 			<p><a href="http://tinyurl.com/owxtkmt">Make a donation today!</a><br />Each donation matters - donate today!</p>
 			<div class='donations'>
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-				<div>
-				<input type="hidden" name="cmd" value="_s-xclick" />
-				<input type="hidden" name="hosted_button_id" value="8490399" />
-				<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" name="submit" alt="Donate" />
-				<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-				</div>
-			</form>
+			 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_donations">
+<input type="hidden" name="business" value="vivacityinfotech@gmail.com">
+<input type="hidden" name="lc" value="US">
+<input type="hidden" name="item_name" value="Plugin & Theme Development">
+<input type="hidden" name="no_note" value="0">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
 			<div class="fb-like" data-href="https://www.facebook.com/vivacityinfotech" data-width="50" data-layout="box_count" data-action="like" data-show-faces="true" data-share="false" style="overflow:hidden;"></div>
 			</div>
 		
